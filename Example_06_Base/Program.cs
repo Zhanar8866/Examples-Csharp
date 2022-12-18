2dd3e70376197b3a9ed4751689d6bbb430d24e98
Zhanar S <zhan-ct@yandex.ru>
2022-12-18 14:24:38 +0300

    добавлена папка Example 06 Base

---
+++ Example_06_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a=3;
+int b=8;
+int c=15;
+int d=5;
+int e=1;
+int max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+Console.Write("max=");
+Console.WriteLine(max);
+
